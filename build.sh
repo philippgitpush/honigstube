859b506eb52631ff31149e3d8397486d850ac327
philippgitpush <adeleine1412@gmail.com>
2025-07-14 11:40:34 +0200

    bump version, add deployment script

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
 gradle build
+
 cp "e:\Development\honigstube\build\libs\honigstube.jar" "C:\Users\Philipp\Desktop\spigot-dev-server\data\plugins\honigstube.jar"
-docker exec -i spigot-dev-server-mc-1 rcon-cli plugman reload all
-docker exec -i spigot-dev-server-mc-1 rcon-cli say Plugins reloaded
+
+docker exec -i spigot-dev-server-mc-1 rcon-cli plugman reload Honigstube
+docker exec -i spigot-dev-server-mc-1 rcon-cli say Neue Plugin-Version geladen! ^-^
---
+++ deploy.sh
@@ -0,0 +1,4 @@
+gradle build
+
+scp e:/Development/honigstube/build/libs/honigstube.jar serverchen:/home/honigstube/data/plugins/honigstube.jar
+ssh serverchen "docker exec -i honigstube-mc-1 rcon-cli plugman reload Honigstube && docker exec -i honigstube-mc-1 rcon-cli say 'Neue Plugin-Version geladen! ^-^'"
