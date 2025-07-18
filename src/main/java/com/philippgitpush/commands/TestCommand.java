643ba56be8e92720c6e7344cc0d0ffbca794c2f7
philippgitpush <adeleine1412@gmail.com>
2025-07-18 09:04:39 +0200

    remove unused test

--- src/main/java/com/philippgitpush/commands/TestCommand.java
+++ src/main/java/com/philippgitpush/commands/TestCommand.java
@@ -1,11 +1,8 @@
 package com.philippgitpush.commands;
 
-import org.bukkit.attribute.Attribute;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.EntityType;
-import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
 
 public class TestCommand implements CommandExecutor {
@@ -17,10 +14,7 @@ public class TestCommand implements CommandExecutor {
       return true;
     }
 
-    Player player = (Player) sender;
-
-    LivingEntity waypoint = (LivingEntity) player.getWorld().spawnEntity(player.getLocation(), EntityType.ARMOR_STAND);
-    waypoint.getAttribute(Attribute.WAYPOINT_TRANSMIT_RANGE).setBaseValue(100.0);
+    // Player player = (Player) sender;
 
     return true;
   }
