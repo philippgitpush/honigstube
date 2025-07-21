ae191e89ba27a12799443f9aef29c6ec240846af
philippgitpush <adeleine1412@gmail.com>
2025-07-21 18:18:36 +0200

    add paper debug entry point for hot code reloading, adjust build process, add development server compose.yml

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 gradle build
 
-cp "e:\Development\honigstube\build\libs\honigstube.jar" "C:\Users\Philipp\Desktop\spigot-dev-server\data\plugins\honigstube.jar"
+cp ".\build\libs\honigstube.jar" ".\server\plugins\honigstube.jar"
 
 docker exec -i spigot-dev-server-mc-1 rcon-cli plugman reload Honigstube
 docker exec -i spigot-dev-server-mc-1 rcon-cli say Neue Plugin-Version geladen! ^-^
